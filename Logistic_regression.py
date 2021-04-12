24fa6167f0f2aa1417b184c523565e13534c948e
mhe1999 <ehteshami1378@gmail.com>
2021-04-12 22:41:29 +0430

    cost function

--- Logistic_regression.py
+++ Logistic_regression.py
@@ -4,7 +4,7 @@ import math
 import matplotlib.pyplot as plt
 # from statsmodels.stats.outliers_influence import variance_inflation_factor
 
-def load_data(path = "linearRegression_carPrice.csv"):
+def load_data(path = "LogisticRegression_Diabetes.csv"):
     data = pd.read_csv(path)
     dataset = data.values
     return dataset
@@ -12,7 +12,31 @@ def load_data(path = "linearRegression_carPrice.csv"):
 def sigomid(z):
     return 1/(1 + np.exp(-z))
 
-if __name__ == '__main__':
+def hyp(X, theta):
+    t = np.dot(X, theta)
+    h = sigomid(t)
+    return h
 
-    a = np.array([1,2,3])
-    print(sigomid(a))
+def cost_funtion(X, y, theta):
+    m = X.shape[0]
+    h = hyp(X, theta)
+    loss = (-y * np.log(h) - (1-y) * np.log(1-h))
+    cost = 1/m * np.sum(loss)
+    return cost
+
+if __name__ == '__main__':
+    all_data = load_data()
+    X = all_data[0:1, :-1]
+    y = all_data[0:1, -1]
+    y = np.reshape(y, (-1,1))
+    theta = np.zeros((X.shape[1]))
+    theta = np.reshape(theta, (-1,1))
+    # print(X.shape)
+    # print(y.shape)
+    # print(theta.shape)
+    # print("x:", X)
+    # print("y:", y)
+    # print("theta:", theta)
+    # print(cost_funtion(X, y, theta).shape)
+    print(cost_funtion(X, y, theta))
+    # print(data.shape[1])
