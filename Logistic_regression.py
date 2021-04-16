484d401e15d2eafaf175c43cac5688d6e25bfe70
mhe1999 <ehteshami1378@gmail.com>
2021-04-16 17:45:34 +0430

    draw_boundary

--- Logistic_regression.py
+++ Logistic_regression.py
@@ -73,8 +73,8 @@ def model(X, y, w, b, itteration = 1500, alpha = 0.01):
         cost = compute_cost(X, y, w, b)
         # print(i, cost)
         if i % 100 == 0:
-            print(i, cost)
-            # pass
+            # print(i, cost)
+            pass
         cost_save= np.append(cost_save, cost)
     return cost_save, w, b, dw, db
 
@@ -101,48 +101,89 @@ def partition_data(dataset,train_percent = 0.8, test_percent = 0.2):
 
 def map_features(X, degree):
     out = X
-    for i in range(X.shape[1]):
-        for j in range(i+1, X.shape[1]):
-            for k in range(degree+1):
-                # print(i,"^",k, j, "^", degree - k)
-                out = np.append(out, np.reshape(np.power(X[:,i], k) * np.power(X[:,j], degree - k), (-1,1)), axis = 1)
+    if X.shape[1] != 1:
+        for i in range(X.shape[1]):
+            for j in range(i+1, X.shape[1]):
+                for k in range(degree+1):
+                    # print(i,"^",k, j, "^", degree - k)
+                    out = np.append(out, np.reshape(np.power(X[:,i], k) * np.power(X[:,j], degree - k), (-1,1)), axis = 1)
+
+    else:
+        for i in range(2,degree):
+            # print("x^",i)
+            out = np.append(out, np.reshape(np.power(X[:,0], i), (-1,1)), axis = 1)
+
     return out
-# out = np.append(X, np.reshape(X[:,0] * X[:,1], (-1,1)), axis = 1)
+
+def final_model(X, X_test, Y, Y_test, degree, alpha, check=0):
+    if check == 1:
+        X = np.reshape(X, (-1,1))
+        X_test = np.reshape(X_test, (-1,1))
+    Y = np.reshape(Y, (-1,1))
+    Xm = map_features(X, degree)
+    X_testm = map_features(X_test, degree)
+    n = Xm.shape[1]
+    w, b = initialize_with_zeros(n)
+
+    X_norm, X_mu, X_sigma = feature_normalization(Xm)
+    # X_norm = X
+    cost_save, w, b, dw, db = model(X_norm, Y, w, b, 5000, alpha)
+    # X_mi
+    Y_prediction_test = predict(X_testm, w, b, X_mu, X_sigma)
+    Y_prediction_train = predict(Xm, w, b, X_mu, X_sigma)
+    print('\ndegree, alpha:', degree, alpha)
+    print("train accuracy: {} %".format(100 - np.mean(np.abs(Y_prediction_train - Y)) * 100))
+    print("test accuracy: {} %".format(100 - np.mean(np.abs(Y_prediction_test - Y_test)) * 100))
+    print("cost:", cost_save[-1])
+    return cost_save, w, b, dw, db, X_norm, X_mu, X_sigma
+
+def draw_boundary(degree, w, b, train_data, X_norm):
+    xxx = yyy = np.linspace(-4,4,10)
+    for i in range(xxx.shape[0]):
+        for j in range(yyy.shape[0]):
+            # xxx = np.reshape(xxx[i], (-1,1))
+            zz = np.array([[xxx[i],yyy[j]]])
+            zzz = map_features(zz,degree)
+            if np.dot(zzz, w) + b > 0:
+                plt.plot(xxx[i], yyy[j], 'xr', alpha=0.3)
+            else:
+                plt.plot(xxx[i], yyy[j], 'xb', alpha=0.3)
+
+        red = np.where(train_data[:, -1] == 1)
+    blue = np.where(train_data[:, -1] == 0)
+    plt.plot(X_norm[red, 0], X_norm[red, 1] ,'or', alpha=0.3)
+    plt.plot(X_norm[blue, 0], X_norm[blue, 1] ,'ob', alpha=0.3)
+
+    xxx = yyy = np.linspace(-4,4,100)
+    z = np.zeros((xxx.shape[0], yyy.shape[0]))
+    # print(xxx)
+    # print(yyy)
+    # print(z)
+    for i in range(xxx.shape[0]):
+        for j in range(yyy.shape[0]):
+            # xxx = np.reshape(xxx[i], (-1,1))
+            zz = np.array([[xxx[i],yyy[j]]])
+            zzz = map_features(zz,degree)
+            z[i,j] = np.dot(zzz, w) + b
+    # print(z)
+    plt.contour(xxx,yyy,z.T)
+
+
+    plt.show()
+
 
 if __name__ == '__main__':
     all_data = load_data()
     train_data , test_data = partition_data(all_data)
     m = train_data.shape[0]
+    d = 22
+    for degree in [d]:
+        for alpha in [0.1]:
 
-    X = train_data[:, :-1]
-    Y = train_data[:, -1]
-    X_test = test_data[:, :-1]
-    Y_test = test_data[:, -1]
-    Y = np.reshape(Y, (-1,1))
-    # X = np.reshape(X, (-1,1))
-    # X_test = np.reshape(X_test, (-1,1))
+            X = np.append(np.reshape(train_data[:, 1], (-1,1)), np.reshape(train_data[:, 5], (-1,1)), axis = 1)
+            Y = train_data[:, -1]
+            X_test = np.append(np.reshape(test_data[:, 1], (-1,1)), np.reshape(test_data[:, 5], (-1,1)), axis = 1)
 
+            Y_test = test_data[:, -1]
 
-    # X_norm, X_mu, X_sigma = feature_normalization(X)
-
-
-    X = map_features(X, 10)
-    X_test = map_features(X_test, 10)
-    print(X.shape)
-    n = X.shape[1]
-    w, b = initialize_with_zeros(n)
-
-    X_norm, X_mu, X_sigma = feature_normalization(X)
-    cost_save, w, b, dw, db = model(X_norm, Y, w, b, 5000, 0.8)
-
-    Y_prediction_test = predict(X_test, w, b, X_mu, X_sigma)
-    Y_prediction_train = predict(X, w, b, X_mu, X_sigma)
-    print("train accuracy: {} %".format(100 - np.mean(np.abs(Y_prediction_train - Y)) * 100))
-    print("test accuracy: {} %".format(100 - np.mean(np.abs(Y_prediction_test - Y_test)) * 100))
-    print("cost:", cost_save[-1])
-    print(X.shape)
-
-    # plt.plot(X, Y, 'o')
-    # plt.show()
-    plt.plot(cost_save)
-    plt.show()
+            cost_save, w, b, dw, db, X_norm, X_mu, X_sigma = final_model(X, X_test, Y, Y_test, degree, alpha)
